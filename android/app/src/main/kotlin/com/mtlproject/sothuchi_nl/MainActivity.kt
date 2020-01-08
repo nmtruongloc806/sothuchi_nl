7de4399f8331321365c8ce7b192bbb41531495b1
truongloc <nmtruongloc806@gmail.com>
2020-01-08 11:17:04 +0700

    commit source basic

---
+++ android/app/src/main/kotlin/com/mtlproject/sothuchi_nl/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mtlproject.sothuchi_nl
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
